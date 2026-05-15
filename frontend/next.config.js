2026576b28d6e08a90778367de6636cdff09ab12
Rohit Biruli <your-email@gmail.com>
2026-05-15 13:27:13 +0530

    chore: remove gunatools domain forwarding

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/tools/image-to-url',
-        destination: 'https://gunatools.dev/tools/image-to-url',
-        permanent: true,
-      },
-    ]
-  },
+
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   images: {
